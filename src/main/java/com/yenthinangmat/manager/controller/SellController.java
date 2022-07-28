f23269c53ece19fec8e0389fba19a4a7f67d3277
31191026916 <92772504+31191026916@users.noreply.github.com>
2022-07-28 20:39:23 +0700

    8:39

--- src/main/java/com/yenthinangmat/manager/controller/SellController.java
+++ src/main/java/com/yenthinangmat/manager/controller/SellController.java
@@ -1,26 +1,31 @@
 package com.yenthinangmat.manager.controller;
 
 import com.yenthinangmat.manager.dto.CategoryDTO;
+import com.yenthinangmat.manager.dto.CustomerDTO;
 import com.yenthinangmat.manager.service.CategoryService;
+import com.yenthinangmat.manager.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @Controller
 public class SellController {
     private final CategoryService categoryService;
+    private final CustomerService customerService;
 
-    public SellController(CategoryService categoryService) {
+    public SellController(CategoryService categoryService, CustomerService customerService) {
         this.categoryService = categoryService;
+        this.customerService = customerService;
     }
 
     @GetMapping("/employee/sell")
     public String sellProduct(Model model){
         List<CategoryDTO> categoryList= categoryService.getAll();
+        List<CustomerDTO> customerL=customerService.getAll();
         model.addAttribute("categoryList",categoryList);
+        model.addAttribute("customerL",customerL);
         return "Sell/index";
     }
 }
